1fe9c1d3f66fa08c04a00d3ed2a8b24f6e460e29
maxPow1776 <maks.greb2@gmail.com>
2023-01-30 22:38:26 +0300

    decompos webpack config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,24 @@
+import webpack from 'webpack';
+import path from 'path';
+import { BuildOptions } from './types/config';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+  const { mode, paths } = options;
+  return {
+    mode,
+    entry: paths.entry,
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+    output: {
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true
+    },
+    plugins: buildPlugins(options)
+  }
+}

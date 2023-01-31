aa821d6d0065026344095f42c0ba1af60d5a76b8
maxPow1776 <maks.greb2@gmail.com>
2023-01-31 22:13:19 +0300

    add react-router and lazy loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { Counter } from './components/Counter';
 
 render(
-  <div>
+  <BrowserRouter>
     <App />
-  </div>,
+  </BrowserRouter>,
   document.getElementById('root')
 )

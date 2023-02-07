353fd5fd444332c40a958bdf0ef5fb23846d9467
maxPow1776 <maks.greb2@gmail.com>
2023-02-07 20:23:01 +0300

    decomposed the router

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+
+const AppRouter = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {Object.values(routeConfig).map(({ path, element }) => {
+            <Route
+              key={path}
+              path={path}
+              element={(
+                <Suspense fallback={<div>Loading...</div>}>
+                  {element}
+                </Suspense>
+              )}
+            />
+          })}
+        </Routes>
+      </Suspense>
+  );
+};
+
+export default AppRouter;

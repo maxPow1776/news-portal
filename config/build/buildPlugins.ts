cd12e3d58d9b1008ff26a7031c265ed703dbbe0b
maxPow1776 <maks.greb2@gmail.com>
2023-02-08 20:24:52 +0300

    add hotModuleReplacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,7 +1,8 @@
 import { BuildOptions } from './types/config';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
@@ -15,6 +16,8 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    ...(isDev ? [new ReactRefreshWebpackPlugin()] : []),
+    new webpack.HotModuleReplacementPlugin(),
   ];
 }

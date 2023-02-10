f35b30aa20849a14c73ff432c95d9577b4127c04
maxPow1776 <maks.greb2@gmail.com>
2023-02-10 10:52:14 +0300

    add nonFoundPage and loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,25 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import { Suspense } from 'react';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
 const AppRouter = () => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <Routes>
-      {Object.values(routeConfig).map(({ path, element }) => (
-        <Route
-          key={path}
-          path={path}
-          element={(
-            <Suspense fallback={<div>Loading...</div>}>
-              <div className="page-wrapper">
-                {element}
-              </div>
-            </Suspense>
+  <Routes>
+    {Object.values(routeConfig).map(({ path, element }) => (
+      <Route
+        key={path}
+        path={path}
+        element={(
+          <Suspense fallback={<PageLoader />}>
+            <div className="page-wrapper">
+              {element}
+            </div>
+          </Suspense>
               )}
-        />
-      ))}
-    </Routes>
-  </Suspense>
+      />
+    ))}
+  </Routes>
 );
 
 export default AppRouter;
---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import classes from './NotFoundPage.module.scss';
+
+export interface NotFoundPageProps {
+  className?: string;
+}
+
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(classes.notFoundPage, {}, [className])}>
+      {t('pageNotFound')}
+    </div>
+  );
+};

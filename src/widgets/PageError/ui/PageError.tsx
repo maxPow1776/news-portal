f15d4acddbc8893e25216b8f68a172f75b62ff75
maxPow1776 <maks.greb2@gmail.com>
2023-02-13 17:56:26 +0300

    add ErrorBoundary

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,24 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import classes from './PageError.module.scss';
+
+export interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError = ({ className }: PageErrorProps) => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+  };
+
+  return (
+    <div className={classNames(classes.pageError, {}, [className])}>
+      <p>{t('unexpectedError')}</p>
+      <Button onClick={reloadPage}>{t('refreshPage')}</Button>
+    </div>
+  );
+};

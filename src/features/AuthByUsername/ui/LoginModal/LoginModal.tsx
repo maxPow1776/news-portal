a44a57fadb5d2c2b2f2c04bc28f2a2314d4e3da0
maxPow1776 <maks.greb2@gmail.com>
2023-02-21 22:21:02 +0300

    add husky and fix screenshot tests

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import classes from './LoginModal.module.scss';
 
 export interface LoginModalProps {
   className?: string;
@@ -11,7 +10,7 @@ export interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
   <Modal
-    className={classNames(classes.loginModal, {}, [className])}
+    className={classNames('', {}, [className])}
     isOpen={isOpen}
     onClose={onClose}
     lazy

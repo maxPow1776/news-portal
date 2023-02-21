63cee90025b0d49c987838af5d8ba3dc140e824b
maxPow1776 <maks.greb2@gmail.com>
2023-02-21 19:31:24 +0300

    add redux

---
+++ src/entities/Counter/model/selectors/getCounterValue/getCounterValue.test.ts
@@ -0,0 +1,12 @@
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { DeepPartial } from '@reduxjs/toolkit';
+import { getCounterValue } from './getCounterValue';
+
+describe('getCounterValue.test', () => {
+  test('getCounterValue.test', () => {
+    const state: DeepPartial<StateSchema> = {
+      counter: { value: 10 },
+    };
+    expect(getCounterValue(state as StateSchema)).toEqual(10);
+  });
+});

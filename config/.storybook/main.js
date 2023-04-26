6b8822dc5a4fda8e2cbe56a545779e1051d3bd55
maxPow1776 <maks.greb2@gmail.com>
2023-04-26 13:01:08 +0300

    add theme addon for storybook

--- config/.storybook/main.js
+++ config/.storybook/main.js
@@ -4,9 +4,13 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-essentials",
+      options: { backgrounds: false }
+    },
     "@storybook/addon-interactions",
     'storybook-addon-mock/register',
+    'storybook-addon-themes',
   ],
   "framework": "@storybook/react",
   "core": {
